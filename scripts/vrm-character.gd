4d2e1fdd328c5db9ff767bf5882a708bcbb4e7ad
Jaime Hernandez <jaimevazz@gmail.com>
2025-07-30 12:18:35 -0400

    vrm-character animation condition through dictionary signal and orbit camera

--- scripts/vrm-character.gd
+++ scripts/vrm-character.gd
@@ -1,34 +1,37 @@
 extends CharacterBody3D
 
-@onready var animation_tree: AnimationTree = $AnimationTree
-
 @export_category("Character")
 @export var character_name: String = "Char_Name"
-
 @export var initial_anim: String = "Idle"
+
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_dictionary_signal)
-	#Dialogic.signal_event.connect(_on_dialogic_string_signal)
 	
 func set_animation_condition(condition_name:String, value: bool):
 	# Cancel previous anim
 	if initial_anim != "":
 		var path ="parameters/conditions/" + initial_anim
 		animation_tree.set(path, false)
+		
 	# Construct the parameter path
 	var parameter_path ="parameters/conditions/" + condition_name
+	
+	# Check if the parameter exists
+	# if animation_tree.has_node(parameter_path):
+	# 	print("Successful found condition parameter in AnimationTree called: ", condition_name)
+		#Set the condition value
+		#animation_tree[parameter_path] = value
+	# else:
+	# 	printerr("Error: AnimationTree does not have a condition parameter named ", condition_name)
+	
 	animation_tree.set(parameter_path, value)
 	initial_anim = condition_name
 	
-	# Check if the parameter exissts
-	#if animation_tree.has_node(parameter_path):
-		##Set the condition value
-		#animation_tree[parameter_path] = value
-	#else:
-		#printerr("Error: AnimationTree does not have a condition parameter named ", condition_name)
 		
 func _on_dialogic_dictionary_signal(argument: Dictionary):
+	
 	print("A Dictionary signal was fired from Dialogic: ", argument)
 	if argument["character"] != character_name:
 		pass
@@ -36,15 +39,3 @@ func _on_dialogic_dictionary_signal(argument: Dictionary):
 	if argument["animation"] == null:
 		pass
 	set_animation_condition(argument["animation"], true)
-	
-func _on_dialogic_string_signal(argument: String):
-	print("A String signal was fired via Dialogic: ", argument)
-	match argument:
-		"Dance":
-			# Set the Dance condition to false
-			set_animation_condition("Dance", true)
-			set_animation_condition("Idle", false)
-		"Idle":
-			# Set Idle condition to true
-			set_animation_condition("Dance", false)
-			set_animation_condition("Idle", true)
