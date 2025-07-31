c2a408c87d9e3705665bcf12c826617a5a9efc76
Jaime Hernandez <jaimevazz@gmail.com>
2025-07-31 14:21:50 -0400

    Separation of concerns vrm-animation_tree.gd and vrm-character.gd

--- scripts/vrm-character.gd
+++ scripts/vrm-character.gd
@@ -2,63 +2,22 @@ extends CharacterBody3D
 
 @export_category("Character")
 @export var character_name: String = "Char_Name"
-@export var initial_anim: String = "Idle"
 
-@export var relaxed_base: float = 0.25
-@export var twerk_blend_amount: float = -0.35
-
+@export_category("Animation")
 @onready var animation_tree: AnimationTree = $AnimationTree
 
-var time = 0
-var relaxed_blend_path = "parameters/Idle_BlendTree/Straight_Relaxed/blend_amount"
-var belly_twerk_blend_path = "parameters/Dance_BlendTree/Belly_Twerk/blend_amount"
-
-func _process(delta):
-	time += delta
-	update_anim(time)
-
-func _ready():
-	Dialogic.signal_event.connect(_on_dialogic_dictionary_signal)
-	
-func update_anim(time: float):
-	var fnl = FastNoiseLite.new()
-	var noise = fnl.get_noise_1d(time)
-	var relaxed = clamp(noise / 2 + 0.5 + relaxed_base, 0, 1)
-	animation_tree.set(relaxed_blend_path, relaxed)	
-	
-	var twerk_blend = clamp (noise / 2 + 0.5 + twerk_blend_amount, 0, 1)
-	print("Belly Twerk blend amount: ", twerk_blend)
-	animation_tree.set(belly_twerk_blend_path, twerk_blend)
-	
-func set_animation_condition(condition_name:String, value: bool):
-	# Cancel previous anim
-	if initial_anim != "":
-		var path ="parameters/conditions/" + initial_anim
-		animation_tree.set(path, false)
-		
-	# Construct the parameter path
-	var parameter_path ="parameters/conditions/" + condition_name
-	
-	# Check if the parameter exists
-	# if animation_tree.has_node(parameter_path):
-	# 	print("Successful found condition parameter in AnimationTree called: ", condition_name)
-		#Set the condition value
-		#animation_tree[parameter_path] = value
-	# else:
-	# 	printerr("Error: AnimationTree does not have a condition parameter named ", condition_name)
-	
-	animation_tree.set(parameter_path, value)
-	initial_anim = condition_name
-
-## Process Dictionary signal sent from Dialogic	
-## Take Arguments:
-## Dictionary="{"animation":"Dance","character":"ArchLinux-Chan"}
-func _on_dialogic_dictionary_signal(argument: Dictionary):
-	
-	print("A Dictionary signal was fired from Dialogic: ", argument)
-	if argument["character"] != "All" && argument["character"] != character_name:
-		return
-	print("Animation signal from Dialogic for ", argument["character"])
-	if argument["animation"] == null:
-		return
-	set_animation_condition(argument["animation"], true)
+@export var initial_anim: String = "Idle":
+	set(v):
+		initial_anim = v
+		if (animation_tree):
+			animation_tree.initial_anim = v
+@export var relaxed_base: float = 0.5:
+	set(v):
+		relaxed_base = v
+		if (animation_tree):
+			animation_tree.relaxed_base = v
+@export var twerk_blend_amount: float = 0.5:
+	set(v):
+		twerk_blend_amount = v
+		if (animation_tree):
+			animation_tree.twerk_blend_amount = v
